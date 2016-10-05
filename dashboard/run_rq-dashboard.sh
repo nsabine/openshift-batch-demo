d9a21cf6ff54ec2ef8f3fdcdcbeeb5e6ce2082a2
Nick sabine <nsabine@redhat.com>
2016-10-05 14:09:56 -0400

    fix host/port mismatch typo

--- dashboard/run_rq-dashboard.sh
+++ dashboard/run_rq-dashboard.sh
@@ -3,4 +3,4 @@
 export LC_ALL=en_US.utf8
 export LANG=en_US.utf8
 
-rq-dashboard --port 9181 --redis-host $REDIS_MASTER_SERVICE_HOST --redis-port $REDIS_MASTER_SERVICE_HOST
+rq-dashboard --port 9181 --redis-host $REDIS_MASTER_SERVICE_HOST --redis-port $REDIS_MASTER_SERVICE_PORT
