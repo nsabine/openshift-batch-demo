4dbd1af4ede2d2d2c807f47337f841b30f345e35
Nick sabine <nsabine@redhat.com>
2016-10-05 12:04:08 -0400

    debugging worker

--- worker/app.py
+++ worker/app.py
@@ -1,10 +1,20 @@
+from redis import StrictRedis
 from rq import Connection, Worker
 
+# use the kubernetes service environment variables to 
+#  connect to the redis queue
+
+REDIS_HOST =  os.environ['REDIS_MASTER_SERVICE_HOST']
+REDIS_PORT =  os.environ['REDIS_MASTER_SERVICE_PORT']
+REDIS_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+QUEUES = ['default']
+
+#connection=StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+
 def runworker():
-  redis_url = app.config['REDIS_URL']
-  redis_connection = redis.from_url(redis_url)
+  redis_connection = redis.from_url(REDIS_URL)
   with Connection(redis_connection):
-    worker = Worker(app.config['QUEUES'])
+    worker = Worker(QUEUES)
     worker.work()
 
 if __name__ == '__main__':
